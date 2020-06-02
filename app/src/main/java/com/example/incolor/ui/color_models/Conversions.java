3f1f5aada3282a6a37080c690cd875e08e0dc81f
RomanKuk <kuk2roman@gmail.com>
2020-06-03 00:47:35 +0300

    initial commit for newton fractal

--- app/src/main/java/com/example/incolor/ui/color_models/Conversions.java
+++ app/src/main/java/com/example/incolor/ui/color_models/Conversions.java
@@ -97,4 +97,46 @@ public class Conversions {
     public static String toHex(int r, int g, int b) {
         return "#" + getHexValues(r) + getHexValues(g) + getHexValues(b);
     }
+
+    public static int HSBtoRGB(float hue, float saturation, float brightness, int[] rgb) {
+        if (saturation == 0)
+            return convert(brightness, brightness, brightness, rgb);
+        if (saturation < 0 || saturation > 1 || brightness < 0 || brightness > 1)
+            throw new IllegalArgumentException();
+        hue = hue - (float) Math.floor(hue);
+        int i = (int) (6 * hue);
+        float f = 6 * hue - i;
+        float p = brightness * (1 - saturation);
+        float q = brightness * (1 - saturation * f);
+        float t = brightness * (1 - saturation * (1 - f));
+        switch (i) {
+            case 0:
+                return convert(brightness, t, p, rgb);
+            case 1:
+                return convert(q, brightness, p, rgb);
+            case 2:
+                return convert(p, brightness, t, rgb);
+            case 3:
+                return convert(p, q, brightness, rgb);
+            case 4:
+                return convert(t, p, brightness, rgb);
+            case 5:
+                return convert(brightness, p, q, rgb);
+            default:
+                throw new InternalError("impossible");
+        }
+    }
+
+    private static int convert(float red, float green, float blue, int[] rgb) {
+        if (red < 0 || red > 1 || green < 0 || green > 1 || blue < 0 || blue > 1)
+            throw new IllegalArgumentException("Bad RGB values");
+        int redval = Math.round(255 * red);
+        int greenval = Math.round(255 * green);
+        int blueval = Math.round(255 * blue);
+        int alphaval = Math.round(255 * (float) 0);
+        rgb[0] = redval;
+        rgb[1] = greenval;
+        rgb[2] = blueval;
+        return (alphaval << 24) | (redval << 16) | (greenval << 8) | blueval;
+    }
 }
